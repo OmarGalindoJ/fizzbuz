d0ff71c410e53370d0525f33a1aad76ce52511b7
OmarGalindoJ <ingomargalindo@outlook.com>
2022-04-27 23:15:56 -0500

    Paso 7: Prueba 2 GitHub Action

--- test/Services/ExporerService.test.js
+++ test/Services/ExporerService.test.js
@@ -1,9 +1,9 @@
 const ExplorerService = require ("./../../lib/services/ExplorerService")
 
-describe("Test para explorer service", () => {
-    test("Requerimiento 1: calcular todos los explorers en una misión", () => {
-        const explorers = [{ mission: "node" }];
-        const explorersInNode = ExplorerService.filterByMission(explorers, "node"); 
-        expect (explorersInNode.length).toBe(1);
-    });
-});   
+describe("Test para ExplorerService" , () => {
+    test("Requerimiento 1: Calcular todos los explorers en una misión", () => {
+        const explorers = [{ mission: "node"}];
+        const explorersInNode = ExplorerService.filterByMission(explorers, "node");
+        expect(explorersInNode.length).toBe(1);
+    })
+})  
