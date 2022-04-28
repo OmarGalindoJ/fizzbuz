86c508570200f0ac0d0b407411c5439f7ad3d844
OmarGalindoJ <ingomargalindo@outlook.com>
2022-04-28 00:05:26 -0500

    Paso 12: Prueba 5 de Github Actions tras restablecer archivo Explore Service a su configuración original

--- test/Services/ExporerService.test.js
+++ test/Services/ExporerService.test.js
@@ -4,6 +4,6 @@ describe("Test para ExplorerService" , () => {
     test("Requerimiento 1: Calcular todos los explorers en una misión", () => {
         const explorers = [{ mission: "node"}];
         const explorersInNode = ExplorerService.filterByMission(explorers, "node");
-        expect(explorersInNode.length).toBe(5);
+        expect(explorersInNode.length).toBe(1);
     })
 })  
